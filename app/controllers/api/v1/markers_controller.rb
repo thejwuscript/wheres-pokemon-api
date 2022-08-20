16b936554bc5d375301e260894a4d66b5052cb44
James Wu <ichangjwu@outlook.com>
2022-08-21 01:37:42 +0900

    Reseed and add wrap params

--- app/controllers/api/v1/markers_controller.rb
+++ app/controllers/api/v1/markers_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::MarkersController < ApplicationController
+  wrap_parameters include: [:pageX, :pageY, :name], format: :json
+
   def index
   end
 
-  def create
-    render json: { message: 'No marker data provided' }, status: 400 unless params[:marker]
+  def create  
+    x = marker_params['pageX']
+    y = marker_params['pageY']
+    pokemon_name = marker_params['name']
+    all_pokemon = Pokemon.all
+    result = all_pokemon.any? { |pokemon| pokemon.found?(x.to_i, y.to_i, pokemon_name) }
+    if result
+      puts "You've found #{pokemon_name}!"
+      render json: { message: "You've found #{pokemon_name}!" }
+    else
+      puts "Sorry, no dice."
+      render json: { message: "Sorry, no dice." }
+    end
   end
 
   def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Pokemon.create(name: "Wobbuffet", locationX: 1439, locationY: 170)
+Pokemon.create(name: "Wobbuffet", locationX: 1439, locationY: 170, offset_x: 16, offset_y: 20)
