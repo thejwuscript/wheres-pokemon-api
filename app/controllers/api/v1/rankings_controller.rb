86243ebd75527345e2632f65530fc5cd1286bb95
James Wu <ichangjwu@outlook.com>
2022-08-23 17:12:47 +0900

    Add ranking controller

--- app/controllers/api/v1/rankings_controller.rb
+++ app/controllers/api/v1/rankings_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::RankingsController < ApplicationController
   def index
+    rankings = Ranking.all
+    render json: rankings
   end
 
   def show
   end
 
   def create
+    ranking = Ranking.new(ranking_params)
+    if ranking.save
+      render json: ranking
+    else
+      render json: { errors: ranking.errors }, status: 422
+    end
   end
+
+  private
+
+  def ranking_params
+    params.require(:ranking).permit(:name, :duration, :timerId)
+  end
+
 end
